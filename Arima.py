08693e058ce6b6c98ed13efad8e0a832c320e5ef
simon <simonfong93@gmail.com>
2022-06-11 17:54:32 +0100

    added in arima and lstm

--- Arima.py
+++ Arima.py
@@ -1,32 +1,286 @@
-import numpy as np
 import pandas as pd
+from pandas.plotting import autocorrelation_plot
+from pandas import DataFrame
+from pandas import concat
+import numpy as np
+from math import sqrt
+
+from sklearn.metrics import mean_squared_error
+from statsmodels.tsa.seasonal import seasonal_decompose
+from statsmodels.tsa.stattools import adfuller
+from statsmodels.tsa.stattools import acf
+from statsmodels.tsa.stattools import pacf
+from statsmodels.tsa.arima.model import ARIMA
+from scipy.stats import boxcox
+import matplotlib
+matplotlib.use('TkAgg')
+import seaborn as sns
+sns.set_style("whitegrid")
 import matplotlib.pyplot as plt
+from matplotlib.pylab import rcParams
+# plt.use('TkAgg')
+#https://www.kaggle.com/code/sercanyesiloz/electricity-production-forecasting-arima/notebook
+#https://www.kaggle.com/code/ludovicocuoghi/electric-production-forecast-lstm-sarima-mape-2-5
 
-
-df = pd.read_csv('train_E1GspfA.csv')
-
-###group by date, rather than hourly
-df_2 = df.groupby(['date'])['demand'].sum().reset_index()
-
-##plot the time seres
-
-df_2.set_index('date',inplace=True)
-
-
-# df_2.plot()
-# plt.show()
-
+df = pd.read_csv('Electric_Production.csv')
+df.columns = ['date','value']
+df['date'] = pd.to_datetime(df['date'],infer_datetime_format=True)
+df = df.set_index(['date'])
 ###check for stationality
 from statsmodels.tsa.stattools import adfuller
-def adfuller_test(sales):
-    result=adfuller(sales)
-    labels = ['ADF Test Statistic','p-value','#Lags Used','Number of Observations']
-    for value,label in zip(result,labels):
-        print(label+' : '+str(value) )
+""""#######################################
+check for stationarity
+#######################################
+"""
+def adfuller_test(ts, window=12):
+    # movingAverage = ts.rolling(window).mean()
+    # movingSTD = ts.rolling(window).std()
 
-    if result[1] <= 0.05:
-        print("strong evidence against the null hypothesis(Ho), reject the null hypothesis. Data is stationary")
+    # plt.figure(figsize=(10, 6))
+    # orig = plt.plot(ts, color='cornflowerblue',
+    #                 label='Original')
+    # mean = plt.plot(movingAverage, color='firebrick',
+    #                 label='Rolling Mean')
+    # std = plt.plot(movingSTD, color='limegreen',
+    #                label='Rolling Std')
+    # plt.legend(loc='upper left')
+    # plt.title('Rolling Statistics', size=14)
+    # plt.show(block=False)
+
+    adf = adfuller(ts, autolag='AIC')
+
+    print('ADF Statistic: {}'.format(round(adf[0], 3)))
+    print('p-value: {}'.format(round(adf[1], 3)))
+    print("##################################")
+    print('Critical Values:')
+
+    for key, ts in adf[4].items():
+        print('{}: {}'.format(key, round(ts, 3)))
+    print("##################################")
+
+    if adf[0] > adf[4]["5%"]:
+        print("ADF > Critical Values")
+        print("Failed to reject null hypothesis, time series is non-stationary.")
     else:
-        print("weak evidence against null hypothesis,indicating it is non-stationary ")
+        print("ADF < Critical Values")
+        print("Reject null hypothesis, time series is stationary.")
 
-adfuller_test(df_2['demand'])
+
+adfuller_test(df, window=12)
+""""
+#######################################
+Transform to stationary
+#######################################
+"""
+df_log_scaled = df
+df_log_scaled['value'] = boxcox(df_log_scaled['value'], lmbda=0.0)
+
+plt.figure(figsize = (10,6))
+plt.plot(df_log_scaled, color = 'cornflowerblue')
+plt.xlabel('Date', size = 12)
+plt.ylabel('Electric Production', size = 12)
+plt.title("After Logarithmic Transformation", size = 14)
+plt.show()
+
+
+""""
+#######################################
+Remove trend
+#######################################
+"""
+moving_avg = df_log_scaled.rolling(window=12).mean()
+df_log_scaled_ma = df_log_scaled - moving_avg
+df_log_scaled_ma.dropna(inplace=True)
+plt.figure(figsize = (10,6))
+plt.plot(df_log_scaled_ma, color = 'cornflowerblue')
+plt.xlabel('Date', size = 12)
+plt.ylabel('Electric Production', size = 12)
+plt.title("After Moving Average", size = 14)
+plt.show()
+
+""""
+#######################################
+Exponential Decay transform
+#######################################
+"""
+df_log_scaled_ma_ed = df_log_scaled_ma.ewm(halflife=12, min_periods=0, adjust=True).mean()
+df_lsma_sub_df_lsma_ed = df_log_scaled_ma - df_log_scaled_ma_ed
+plt.figure(figsize = (10,6))
+plt.plot(df_lsma_sub_df_lsma_ed - df_log_scaled_ma_ed, color='cornflowerblue')
+plt.xlabel('Date', size = 12)
+plt.ylabel('Electric Production', size = 12)
+plt.title("After Exponential Decay Transformation", size = 14)
+plt.show()
+
+""""
+#######################################
+Decomposition
+#######################################
+"""
+rcParams['figure.figsize']=10,8
+df_seasonal_decompose = seasonal_decompose(df_lsma_sub_df_lsma_ed,
+                                           model='duplicative')
+df_seasonal_decompose.plot()
+plt.show()
+
+
+""""
+#######################################
+ACF & PACF
+ACF & PACF
+Autocorrelation and partial autocorrelation plots are heavily used in time series analysis and forecasting.
+
+These are plots that graphically summarize the strength of a relationship with an observation in a time series with observations at prior time steps.
+#######################################
+"""
+auto_c_f = acf(df_lsma_sub_df_lsma_ed, nlags=20)
+partial_auto_c_f = pacf(df_lsma_sub_df_lsma_ed, nlags=20, method='ols')
+
+fig, axs = plt.subplots(1, 2, figsize=(12, 5))
+
+plt.subplot(121)
+plt.plot(auto_c_f)
+plt.axhline(y=0, linestyle='--', color='limegreen')
+plt.axhline(y=-1.96 / np.sqrt(len(df_lsma_sub_df_lsma_ed)),
+            linestyle='--', color='firebrick')
+plt.axhline(y=1.96 / np.sqrt(len(df_lsma_sub_df_lsma_ed)),
+            linestyle='--', color='firebrick')
+plt.title('Autocorrelation Function', size=14)
+
+plt.subplot(122)
+plt.plot(partial_auto_c_f)
+plt.axhline(y=0, linestyle='--', color='limegreen')
+plt.axhline(y=-1.96 / np.sqrt(len(df_lsma_sub_df_lsma_ed)),
+            linestyle='--', color='firebrick')
+plt.axhline(y=1.96 / np.sqrt(len(df_lsma_sub_df_lsma_ed)),
+            linestyle='--', color='firebrick')
+plt.title('Partial Autocorrelation Function', size=14)
+
+plt.tight_layout()
+
+""""
+#######################################
+##################FORECASTING##################
+#######################################
+#######################################
+Persistence Model
+#######################################
+"""
+
+values = DataFrame(df_lsma_sub_df_lsma_ed.values)
+persistence_df = concat([values.shift(1), values], axis=1)
+persistence_df.columns = ['t-1', 't+1']
+per_values = persistence_df.values
+
+train = per_values[1:len(per_values)-10]
+test = per_values[len(per_values)-10:]
+
+X_train, y_train = train[:,0], train[:,1]
+X_test, y_test = test[:,0], test[:,1]
+
+def persistence(x):
+    return x
+
+predictions = []
+for i in X_test:
+    y_pred = persistence(i)
+    predictions.append(y_pred)
+
+persistence_score = mean_squared_error(y_test, predictions, squared=False)
+print('Persistence MSE: {}'.format(round(persistence_score,4)))
+
+plt.figure(figsize = (10,6))
+plt.plot(y_test, label = "true values", color = "cornflowerblue")
+plt.plot(predictions,label = "forecasts", color='darkorange')
+plt.title("Persistence Model", size = 14)
+plt.legend(loc = 'upper left')
+plt.show()
+
+
+
+"""
+#######################################
+Autoregression Model
+
+An autoregression model is a linear regression model that uses lagged variables as input variables.
+We could calculate the linear regression model manually using the LinearRegession class in scikit-learn and manually specify the lag input variables to use.
+
+Alternately, the statsmodels library provides an autoregression model where you must specify an appropriate lag value and trains a linear regression model. It is provided in the AutoReg class.
+#######################################
+"""
+ar_values = df_lsma_sub_df_lsma_ed.values
+train = ar_values[1:len(ar_values)-10]
+test = ar_values[len(ar_values)-10:]
+model = ARIMA(train, order=(2,1,0))
+AR_model = model.fit()
+
+predictions = AR_model.predict(start=len(train), end=len(train)+len(test)-1, dynamic=False)
+ar_score = mean_squared_error(test, predictions, squared=False)
+print('AR MSE: {}'.format(round(ar_score,4)))
+
+plt.figure(figsize = (10,6))
+plt.plot(test, label = "true values", color = "cornflowerblue")
+plt.plot(predictions,label = "forecasts", color='darkorange')
+plt.title("AR Model", size = 14)
+plt.legend(loc = 'upper left')
+plt.show()
+
+
+"""
+#######################################
+Moving Average Model
+#######################################
+"""
+
+model = ARIMA(train, order=(0,1,2))
+MA_model = model.fit()
+
+predictions = MA_model.predict(start=len(train), end=len(train)+len(test)-1, dynamic=False)
+ma_score = mean_squared_error(test, predictions, squared=False)
+print('MA MSE: {}'.format(round(ma_score,4)))
+
+plt.figure(figsize = (10,6))
+plt.plot(test, label = "true values", color = "cornflowerblue")
+plt.plot(predictions,label = "forecasts", color='darkorange')
+plt.title("MA Model", size = 14)
+plt.legend(loc = 'upper left')
+plt.show()
+
+"""
+#######################################
+ARIMA Model
+
+The statsmodels library provides the capability to fit an ARIMA model.
+An ARIMA model can be created using the statsmodels library as follows:
+
+1. Define the model by calling ARIMA() and passing in the p, d, and q parameters.
+2. The model is prepared on the training data by calling the fit() function.
+3. Predictions can be made by calling the predict() function and specifying the index of the time or times to be predicted.
+
+Let’s start off with something simple. We will fit an ARIMA model to the entire Electric Production dataset.
+#######################################
+"""
+model = ARIMA(train, order=(2,1,2))
+ARIMA_model = model.fit()
+
+predictions = ARIMA_model.predict(start=len(train), end=len(train)+len(test)-1, dynamic=False)
+arima_score = mean_squared_error(test, predictions, squared=False)
+print('ARIMA MSE: {}'.format(round(arima_score,4)))
+
+plt.figure(figsize = (10,6))
+plt.plot(test, label = "true values", color = "cornflowerblue")
+plt.plot(predictions,label = "forecasts", color='darkorange')
+plt.title("ARIMA Model", size = 14)
+plt.legend(loc = 'upper left')
+plt.show()
+
+
+"""
+#######################################
+Mean Squared Errors
+#######################################
+"""
+errors = pd.DataFrame()
+errors["Model"] = ["Persistence", "Autoregression", "Moving Average", "ARIMA"]
+errors["MSE"] = [persistence_score, ar_score, ma_score, arima_score]
+errors = errors.sort_values("MSE", ascending = True, ignore_index = True)
---
+++ LSTM.py
@@ -0,0 +1,260 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+##https://www.kaggle.com/code/ludovicocuoghi/electric-production-forecast-lstm-sarima-mape-2-5
+import torch
+import torch.nn as nn
+from torch.autograd import Variable
+
+from statsmodels.tsa.statespace.sarimax import SARIMAX
+from statsmodels.tsa.seasonal import seasonal_decompose
+from statsmodels.tsa.stattools import adfuller
+from statsmodels.graphics.tsaplots import plot_acf, plot_pacf
+
+from sklearn.preprocessing import MinMaxScaler
+from sklearn.metrics import mean_squared_error
+
+
+from math import sqrt
+
+plt.style.use("seaborn-whitegrid")
+plt.rc("figure", autolayout=True)
+plt.rc("axes", labelweight="bold", labelsize="large", titleweight="bold", titlesize=14, titlepad=10)
+
+
+
+df = pd.read_csv('Electric_Production.csv')
+df.columns = ['date','value']
+df['date'] = pd.to_datetime(df['date'],infer_datetime_format=True)
+df = df.set_index(['date'])
+
+"""
+The first thing we must do is to properly shape the input data. 
+When modeling a time series by LSTM RNN, it is crucial to to properly shape the 
+input data in a sliding windows format. In this application, 
+the data is given as monthly data. So, for example, 
+we can use a 12 steps prediction window. This means that we use 12 samples of 
+data (data of an entire year) to predict the 13th sample.
+"""
+DEVICE = 'cuda' if torch.cuda.is_available() else 'cpu'
+
+prediction_window=12
+
+n_input=12 #Use 12 months data to predict 13 th month data
+n_features=1 # we are dealing with an univariate time series, so n_features should be set to 1.
+#In case of a multivariate time series, n_features should be set to a proper value higher than 1.
+
+
+train = df.copy()
+
+
+"""
+########################
+## rescale data
+########################
+"""
+scaler = MinMaxScaler()
+scaler.fit(train)
+scaled_train = scaler.transform(train)
+
+
+"""
+Now we can formally create the train set. As stated before, the train set will have 
+a "sliding window" shape. This means that we have to shape the data in such a way that
+ the RNN will predict the 13th sample starting from the previous from 12 samples.
+"""
+def sliding_windows(data, n_input):
+    X_train=[]
+    y_train=[]
+    for i in range(n_input,len(data)):
+        X_train.append(data[i-n_input:i])
+        y_train.append(data[i])
+    return np.array(X_train), np.array(y_train)
+
+
+x, y = sliding_windows(scaled_train, prediction_window)
+
+print(f'Given the Array: \n {x[0].flatten()}')
+print(f'Predict this value: \n {y[0]}')
+
+
+train_size = int(len(train) - prediction_window*3)
+val_size = len(train) - train_size
+
+"""
+########################
+## convert to tensor
+########################
+"""
+dataX = Variable(torch.Tensor(np.array(x)))
+dataY = Variable(torch.Tensor(np.array(y)))
+
+X_train = Variable(torch.Tensor(np.array(x[:train_size])))
+y_train = Variable(torch.Tensor(np.array(y[:train_size])))
+
+X_valid = Variable(torch.Tensor(np.array(x[train_size:len(x)])))
+y_valid = Variable(torch.Tensor(np.array(y[train_size:len(y)])))
+
+
+dataX.to(DEVICE)
+dataY.to(DEVICE)
+X_train.to(DEVICE)
+y_train.to(DEVICE)
+X_valid.to(DEVICE)
+y_valid.to(DEVICE)
+
+"""
+########################
+## LSTM Modelling
+########################
+"""
+
+
+class LSTMNet(nn.Module):
+    def __init__(self, input_size, hidden_size, num_layers):
+        super(LSTMNet, self).__init__()
+        self.input_size = input_size
+        self.hidden_size = hidden_size
+        self.num_layers = num_layers
+
+        self.lstm = nn.LSTM(
+            input_size=input_size, hidden_size=hidden_size,
+            num_layers=num_layers, batch_first=True
+        )
+
+        self.fc1 = nn.Linear(hidden_size, 40)
+        self.fc2 = nn.Linear(40, 1)
+        self.relu = nn.ReLU()
+
+    def forward(self, x):
+        h0 = Variable(torch.zeros(self.num_layers, x.size(0), self.hidden_size))
+        c0 = Variable(torch.zeros(self.num_layers, x.size(0), self.hidden_size))
+        _, (h_out, _) = self.lstm(x, (h0, c0))
+
+        h_out = h_out.view(-1, self.hidden_size)
+
+        out = self.fc2(self.relu(self.fc1(h_out)))
+
+        return out
+
+
+"""
+########################
+## Parameters for training
+########################
+"""
+
+EPOCHS = 2000
+LEARNING_RATE = 0.008
+
+INPUT_SIZE = n_features
+HIDDEN_SIZE = 100
+NUM_LAYERS = 1
+
+model = LSTMNet(INPUT_SIZE, HIDDEN_SIZE, NUM_LAYERS)
+model.to(DEVICE)
+print(model)
+
+criterion = torch.nn.MSELoss()
+optimizer = torch.optim.Adam(model.parameters(), lr = LEARNING_RATE)
+
+"""
+########################
+## training 
+########################
+"""
+
+early_stopping_patience = 150
+early_stopping_counter = 0
+
+valid_loss_min = np.inf
+
+for epoch in range(EPOCHS):
+    optimizer.zero_grad()
+    model.train()
+    output = model(X_train)
+
+    train_loss = criterion(output, y_train)
+
+    train_loss.backward()
+    optimizer.step()
+
+    with torch.no_grad():
+        model.eval()
+        output_val = model(X_valid)
+        valid_loss = criterion(output_val, y_valid)
+
+        if valid_loss <= valid_loss_min:
+            torch.save(model.state_dict(), './state_dict.pt')
+            print(
+                f'Epoch {epoch + 0:01}: Validation loss decreased ({valid_loss_min:.6f} --> {valid_loss:.6f}).  Saving model ...')
+            valid_loss_min = valid_loss
+            early_stopping_counter = 0  # reset counter if validation loss decreases
+        else:
+            print(f'Epoch {epoch + 0:01}: Validation loss did not decrease')
+            early_stopping_counter += 1
+
+        if early_stopping_counter > early_stopping_patience:
+            print('Early stopped at epoch :', epoch)
+            break
+
+        print(f'\t Train_Loss: {train_loss:.4f} Val_Loss: {valid_loss:.4f}  BEST VAL Loss: {valid_loss_min:.4f}\n')
+
+"""
+########################
+## LSTM Prediction
+########################
+"""
+
+# Loading the best model
+model.load_state_dict(torch.load('./state_dict.pt'))
+
+valid_predict = model(X_valid)
+y_pred_scaled = valid_predict.data.numpy()
+y_pred = scaler.inverse_transform(y_pred_scaled)
+
+df_pred = train.iloc[-24:]
+df_pred['prediction'] = y_pred
+
+# mape_lstm = mape(df_pred["value"], df_pred["prediction"])
+# print(f"MAPE OF LSTM MODEL : {mape_lstm:.2f} %")
+
+rmse_lstm = mean_squared_error(df_pred["value"], df_pred["prediction"], squared=False)
+print(f"RMSE OF LSTM MODEL : {rmse_lstm:.2f}")
+
+
+"""
+    ########################
+    ## LSTM Forecasting
+    ########################
+"""
+
+test_predictions = []
+
+first_eval_batch = Variable(torch.Tensor(scaled_train[-n_input:]))  # use the previous 12 samples to predict the 13th
+current_batch = first_eval_batch.reshape((1, n_input, n_features))  # reshape the data into (1,12,1)
+for i in range(len(scaled_train[-n_input:])):
+    # get the prediction value for the first batch
+    current_pred = model(current_batch)
+    # append the prediction into the array
+    test_predictions.append(current_pred)
+
+    # use the prediction to update the batch and remove the first value
+    current_batch = torch.cat((current_batch[:, 1:, :], current_pred.reshape(1, 1, 1)), 1)
+
+forec_vals = [val.flatten().item() for val in test_predictions]
+forec_vals = np.array(forec_vals).reshape(-1,1)
+forec_vals = scaler.inverse_transform(forec_vals)
+
+date_offset=12
+forecast_dates =  (train.index + pd.DateOffset(months=date_offset))[-date_offset:]
+forecast_dates
+
+
+df_forecast=pd.DataFrame({'date': forecast_dates})
+df_forecast.set_index('date', inplace=True)
+df_forecast['prediction'] = forec_vals
+df_forecast.head(12)
+
+df_full=df_pred.append(df_forecast)
